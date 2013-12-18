e0924dce026b891f65359f2516b8909aeef8a05a
Ramnath <k.ramnath@gmail.com>
2013-12-18 17:46:14 +0530

    moving out constants to settings file and consolidating output into just 2 csv files

--- scraper.py
+++ scraper.py
@@ -3,23 +3,35 @@ import os
 import glob
 import json
 import csv
+import settings
 
-dir_name = "/Users/ramnathkrishnamurthy/Dropbox/Pingpong/LDMI/Christmas Campaign/User List Htmls/Cognizant Christians Old/"
-output_dir_name = "/Users/ramnathkrishnamurthy/Dropbox/Pingpong/LDMI/Christmas Campaign/User List/"
-for page in glob.glob(dir_name + "*.htm"):
-  page_name = os.path.basename(page)
-  print page_name
-  soup = BeautifulSoup(open(page))
-  container_tag = soup.select("div._4_yl > div")
-  with open(output_dir_name + os.path.splitext(page_name)[0] + ".csv", "wb") as csvfile:
+input_dir_name = settings.input_dir_name
+output_dir_name = settings.output_dir_name
+output_file_prefix = settings.output_file_prefix
+
+for output_file_name in ["male", "female"]:
+  with open(output_dir_name + output_file_prefix + output_file_name + ".csv", "wb") as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=",")
     csvwriter.writerow(["Name", "fbuid"])
+
+for page in glob.glob(input_dir_name + "*.htm*"):
+  page_name = os.path.splitext(os.path.basename(page))[0] 
+  print page_name
+  if page_name.endswith("female"):
+    output_file_name = "female"
+  else:
+    output_file_name = "male"
+  soup = BeautifulSoup(open(page))
+  container_tag = soup.select("div._4_yl > div")
+  with open(output_dir_name + output_file_prefix + output_file_name + ".csv", "ab") as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=",")
     for tag in container_tag:
       if tag.has_attr("data-bt"):
         data = tag.get("data-bt")
         data = json.loads(data)
+        fbuid = data.get("id")
         name_tag = tag.select("div._zs > a")[0]
         name = name_tag.contents[0]
-        print name, data.get("id") 
-        csvwriter.writerow([name, data.get("id")])
+        print name, fbuid
+        csvwriter.writerow([name, fbuid])
   
---
+++ settings.py
@@ -0,0 +1,3 @@
+input_dir_name = "/Users/ramnathkrishnamurthy/Dropbox/Pingpong/LDMI/Christmas Campaign/User List Htmls/Cognizant Christians New/"
+output_dir_name = "/Users/ramnathkrishnamurthy/Dropbox/Pingpong/LDMI/Christmas Campaign/User List/"
+output_file_prefix = "cts_christians_"
